ca4c57bdc4d83eb3f828d4bec14bb258297e6e4c
Carlos Gomez <gomezmendozacarlos>
2025-01-28 18:03:20 -0600

    Adds events router and update root router

---
+++ routes/events.router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    bands: [],
+    cost: {
+      dia: "200",
+      pre: "150",
+      vip: "250"
+    },
+    date: "2023-01-01",
+    flyer: "",
+    time: "20:00",
+    venue: {
+      address: "Epigmenio Gonz lez 66, Centro, 44100 Guadalajara, Jal.",
+      capacity: 2000,
+      city: "Guadalajara",
+      name: "Foro Independencia",
+      state: "Jalisco"
+    },
+  });
+})
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const bandsRouter = require('./bands.router');
 const venuesRouter = require('./venues.router');
 const usersRouter = require('./users.router');
+const eventsRouter = require('./events.router');
 
 function routerApi(appRouter) {
   appRouter.use('/bands', bandsRouter);
   appRouter.use('/venues', venuesRouter);
   appRouter.use('/users', usersRouter);
+  appRouter.use('/events', eventsRouter);
 
   appRouter.get('/', (req, res) => {
     res.send('Hello World!');
