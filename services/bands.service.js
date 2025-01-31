87b58b5e39e0e855a0b23889f2ac24e248b62a9f
Carlos Gomez <gomezmendozacarlos>
2025-01-30 19:31:34 -0600

    Update: bands service to query

--- services/bands.service.js
+++ services/bands.service.js
@@ -1,67 +1,70 @@
-const { faker } = require('@faker-js/faker');
+const pool = require('../libs/postgres.pool');
 
 class ProductsService {
 
   constructor(){
-    this.bands = [];
-    this.generate();
-  }
-
-  generate() {
-    const limit = 100;
-    for (let index = 0; index < limit; index++) {
-      this.bands.push({
-        id: faker.string.uuid(),
-        name: faker.music.artist(),
-        genre: faker.music.genre(),
-        image: faker.image.url(),
-      });
-    }
-  }
-
-  async create(data) {
-    const newBand = {
-      id: faker.string.uuid(),
-      ...data
-    }
-    this.bands.push(newBand);
-    return newBand;
-  }
-
-  find() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(this.bands);
-      });
+    this.pool = pool;
+    this.pool.on('error', (err) => {
+      console.error('Unexpected error on idle client', err);
+      process.exit(-1);
     })
   }
 
-  async findOne(id) {
-    return this.bands.find(item => item.id === id);
+  async create(data) {
+    const { name, location, genre } = data;
+    const query = `
+      INSERT INTO bands (name, location, genre)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const values = [name, location, genre];
+    const { rows } = await this.pool.query(query, values);
+    return rows[0];
   }
 
-  async update(id, changes) {
-    const index = this.bands.findIndex(item => item.id === id);
-    if (index === -1) {
-      throw new Error('Banda no encontrada');
-    }
-    const product = this.bands[index];
-    this.bands[index] = {
-      ...product,
-      ...changes
-    };
-    return this.bands[index];
+  async find() {
+    const query = 'SELECT * FROM bands';
+    const { rows } = await this.pool.query(query);
+
+    return rows
   }
+
+  async findOne(id) {
+    const query = `SELECT * FROM bands WHERE id = ${id}`;
+    const { rows } = await this.pool.query(query);
+    return rows;
+  }
+
+    async update(id, changes) {
+      const { name, genre, location } = changes;
+      const query = `
+        UPDATE bands
+        SET name = $1, genre = $2, location = $3
+        WHERE id = $4
+        RETURNING *;
+      `;
+      const values = [name, genre, location, id];
+      const { rows } = await this.pool.query(query, values);
+      if (rows.length === 0) {
+        throw new Error('Banda no encontrada');
+      }
+      return rows[0];
+    }
 
   async delete(id) {
-    const index = this.bands.findIndex(item => item.id === id);
-    if (index === -1) {
+    const query = `
+      DELETE FROM bands
+      WHERE id = $1
+      RETURNING *;
+    `;
+    const values = [id];
+    const { rows } = await this.pool.query(query, values);
+    if (rows.length === 0) {
       throw new Error('Banda no encontrada');
     }
-    this.bands.splice(index, 1);
-    return { id };
+    return rows[0];
   }
-
 }
+
 
 module.exports = ProductsService;
